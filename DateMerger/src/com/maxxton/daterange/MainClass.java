caceb89ef23152ee658f034ab6c1c7e917c0807c
ujwal <ujwal@ujwal>
2020-11-11 11:54:26 +0530

    Change hard-coded values to dynamic values

--- DateMerger/src/com/maxxton/daterange/MainClass.java
+++ DateMerger/src/com/maxxton/daterange/MainClass.java
@@ -5,65 +5,36 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) throws ParseException {
 		
-		 SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH);
-		 dateFormat.setLenient(false);
-		 
-		 DateRange daterange1=new DateRange();
-		 daterange1.setStartDate(dateFormat.parse("01/01/2020"));
-		 daterange1.setEndDate(dateFormat.parse("30/01/2020"));
-		 
-		 DateRange daterange2=new DateRange();
-		 daterange2.setStartDate(dateFormat.parse("15/01/2020"));
-		 daterange2.setEndDate(dateFormat.parse("20/03/2020"));
-		 
-		 DateRange daterange3=new DateRange();
-		 daterange3.setStartDate(dateFormat.parse("10/03/2020"));
-		 daterange3.setEndDate(dateFormat.parse("15/04/2020"));
-		 
-		 DateRange daterange4=new DateRange();
-		 daterange4.setStartDate(dateFormat.parse("10/04/2020"));
-		 daterange4.setEndDate(dateFormat.parse("15/05/2020"));
-		 
-		 //date ranges should not be merged
-		 DateRange daterange21=new DateRange();
-		 daterange21.setStartDate(dateFormat.parse("01/01/2020"));
-		 daterange21.setEndDate(dateFormat.parse("30/01/2020"));
-		 
-		 DateRange daterange22=new DateRange();
-		 daterange22.setStartDate(dateFormat.parse("02/01/2020"));
-		 daterange22.setEndDate(dateFormat.parse("20/03/2020"));
-		 
-		 //date ranges should be merged
-		 DateRange daterange31=new DateRange();
-		 daterange31.setStartDate(dateFormat.parse("10/03/2020"));
-		 daterange31.setEndDate(dateFormat.parse("15/04/2020"));
-		 
-		 DateRange daterange32=new DateRange();
-		 daterange32.setStartDate(dateFormat.parse("15/04/2020"));
-		 daterange32.setEndDate(dateFormat.parse("15/05/2020"));
-
 		 List<DateRange> dateRanges=new ArrayList<DateRange>();
-		 dateRanges.add(daterange1);
-		 dateRanges.add(daterange2);
-		 dateRanges.add(daterange3);
-		 dateRanges.add(daterange4);
-		 dateRanges.add(daterange21);
-		 dateRanges.add(daterange22);
-		 dateRanges.add(daterange31);
-		 dateRanges.add(daterange32);
-		 
-		 SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");  
+		 String startDate;
+		 String endDate;
+		 SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH);
+		 dateFormat.setLenient(false);  
+		 Scanner scanner=new Scanner(System.in);		 
+		 for(int i=0;i<2;i++) {
+			 System.out.println("Date Range"+(i+1));
+			 System.out.println("Enter The Start Date(dd/MM/yyyy)=");
+			 startDate=scanner.nextLine();
+			 
+			 System.out.println("Enter The End Date(dd/MM/yyyy)=");
+			 endDate=scanner.nextLine();
+			 
+			 DateRange daterange1=new DateRange();
+			 daterange1.setStartDate(dateFormat.parse(startDate));
+			 daterange1.setEndDate(dateFormat.parse(endDate));
+			 dateRanges.add(daterange1);
+		 }		 
+		 SimpleDateFormat dateFormatter = new SimpleDateFormat("dd MMM yyyy");  
 		 DateMarger dateMarger=new DateMarger();
-		 
-		 System.out.println("\tMarged Dates");
+		 System.out.println("\tMarged Date");
 		 for(DateRange range:dateMarger.mergeDates(dateRanges)){
-			 System.out.println(formatter.format(range.getStartDate())+" - "+formatter.format(range.getEndDate()));
+			 System.out.println(dateFormatter.format(range.getStartDate())+" - "+dateFormatter.format(range.getEndDate()));
 		 }
 	}
-
 }
